d9c01c6c5d4bb4b6557a9f4aedaa1f984f832dc3
jnywong <jnywong.pro@gmail.com>
2024-01-19 17:46:14 +0000

    Split nox sessions into build and preview

--- noxfile.py
+++ noxfile.py
@@ -2,21 +2,34 @@ import nox
 
 nox.options.reuse_existing_virtualenvs = True
 
-build_command = ["./handbook"]
+input_folder = ["./handbook"]
+output_folder = [f"{input_folder[0]}/_build/html"]
 
-@nox.session
-def handbook(session): 
+@nox.session(venv_backend="venv")
+def build(session):
     session.install("-r", "./images/handbook-authoring-image/requirements.txt")
-    if "live" in session.posargs:
-        session.run("jupyter-book", "config", "sphinx", "./handbook")
-        AUTOBUILD_IGNORE = [
-            "_build",
-            "build_assets",
-        ]
-        cmd = ["sphinx-autobuild"]
-        for folder in AUTOBUILD_IGNORE:
-            cmd.extend(["--ignore", f"*/{folder}/*"])
-        cmd.extend(build_command)
-        session.run(*cmd, "./handbook/_build/html")
+    if session.posargs:
+        input_folder = session.posargs
     else:
-        session.run("jupyter-book", "build", *build_command)
+        input_folder = ["./handbook"]
+    session.run("jupyter-book", "config", "sphinx", *input_folder)        
+    session.run("jupyter-book", "build", *input_folder)
+
+@nox.session(venv_backend="venv")
+def preview(session):
+    session.install("-r", "./images/handbook-authoring-image/requirements.txt")
+    if session.posargs:
+        input_folder = session.posargs
+    else:
+        input_folder = ["./handbook"]
+    output_folder = [f"{input_folder[0]}/_build/html"]
+    session.run("jupyter-book", "config", "sphinx", *input_folder)
+    AUTOBUILD_IGNORE = [
+        "_build",
+        "build_assets",
+    ]
+    cmd = ["sphinx-autobuild"]
+    for folder in AUTOBUILD_IGNORE:
+        cmd.extend(["--ignore", f"*/{folder}/*"])
+    cmd.extend([*input_folder, *output_folder])
+    session.run(*cmd)
